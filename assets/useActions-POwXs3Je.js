3c350e0dc6abfa7ddb9b3bc2a748d9041ee37758
Olexandr Lisovyi <microfox141120@outlook.com>
2024-04-20 22:08:24 +0300

    Updates

---
+++ assets/useActions-POwXs3Je.js
@@ -0,0 +1,1 @@
+import{t,v as o,r,w as a}from"./index-CURUI6u3.js";const c={...t},e=()=>{const s=o();return r.useMemo(()=>a(c,s),[s])};export{e as u};
---
+++ assets/useCartActions-wu9ca1bx.js
@@ -0,0 +1,1 @@
+import{o as u}from"./index-CURUI6u3.js";import{u as c}from"./useActions-POwXs3Je.js";const d=(o=null)=>{var r;let s=null;const t=u(e=>e.cart),{toggleCart:n,removeCartItem:a,changeItemQuantity:m}=c();return o&&(s=(r=t==null?void 0:t.items)==null?void 0:r.some(e=>e.id===+o)),{isAdded:s,toggleCart:n,removeCartItem:a,changeItemQuantity:m}};export{d as u};
