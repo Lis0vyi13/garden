032618df0939e716f92576d06dc8421093583b2a
Olexandr Lisovyi <microfox141120@outlook.com>
2024-04-20 21:56:56 +0300

    Updates

--- assets/CateroriesPage-CrOiR4OC.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-Bwev-eBv.js";import{P as e}from"./PageWrapper-6jHYlGTl.js";import{C as s}from"./Categories-DMUgblmM.js";import{F as a}from"./Footer-DETPVb5P.js";const n=()=>r.jsxs(r.Fragment,{children:[r.jsx("main",{className:"main",children:r.jsx(e,{block:"categories",children:r.jsx(s,{})})}),r.jsx(a,{}),";"]});export{n as default};
