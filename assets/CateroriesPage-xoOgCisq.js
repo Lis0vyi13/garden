e770143102c7fe4d083f3e57e2e3ab4cd98804b1
Olexandr Lisovyi <microfox141120@outlook.com>
2024-04-20 18:35:31 +0300

    Updates

---
+++ assets/CateroriesPage-xoOgCisq.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DyWxIyel.js";import{P as e}from"./PageWrapper-yooJQXfr.js";import{C as o}from"./Categories-DkEXNycG.js";const i=()=>r.jsx(e,{block:"categories",children:r.jsx(o,{})});export{i as default};
---
+++ assets/useActions-B_NDGZuj.js
@@ -0,0 +1,1 @@
+import{t,v as o,r,w as a}from"./index-DyWxIyel.js";const c={...t},e=()=>{const s=o();return r.useMemo(()=>a(c,s),[s])};export{e as u};
