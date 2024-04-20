cb11acf63d4a35f02ae12bd83ccf962291c93faa
Olexandr Lisovyi <microfox141120@outlook.com>
2024-04-20 21:37:16 +0300

    Updates

--- assets/CateroriesPage-YQ8cybY-.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CiARpoU3.js";import{P as e}from"./PageWrapper-GnrMzwPM.js";import{C as o}from"./Categories-CXKEGjtI.js";const i=()=>r.jsx(e,{block:"categories",children:r.jsx(o,{})});export{i as default};
